c0c75b8fbdc9e6899808ab5f2fba4c842e375c53
HiTsH <hitsh@hitsh.local>
2023-08-17 21:33:29 +0200

    Initial commit

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,53 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Int "mo:base/Int";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  //stable keyword converts variable to main the latest value not matter app refreshed or reopened
+  // without it the variable returns to it's original value. (orthogonal persistance)
+
+  stable var currentValue : Float = 300;
+
+  // currentValue := 100;
+
+  let id = 234632683264;
+
+  //Debug.print(debug_show (id));
+
+  stable var startTime = Time.now();
+  startTime := Time.now();
+
+  public func topUp(amount : Float) {
+    currentValue += amount;
+
+    Debug.print(debug_show (currentValue));
   };
+  public func withdraw(amount : Float) {
+    let tempValue : Float = currentValue - amount;
+    if (tempValue >= 0) {
+      currentValue -= amount;
+      // Debug.print(debug_show (currentValue));
+    } else {
+      Debug.print("Error!: withdraw amount higher than balance!");
+    };
+  };
+
+  // query method, require 'async' and 'type i.e. Nat/Int/...' and 'return'
+  public query func checkBalance() : async Float {
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElappsedNS = currentTime - startTime;
+    let timeElappsed = timeElappsedNS / 1000000000;
+
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElappsed));
+
+    startTime := currentTime;
+  }
+
+  //topUp();
 };
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,49 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function(){
+  update();
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("form").addEventListener("submit", async function (event) {
+  event.preventDefault();
 
+  // get hold of submit button
+  const button = document.querySelector("#submit-btn");
+
+  // define topup & withdraw amount in variables
+  const inputAmount = parseFloat(document.querySelector("#input-amount").value);
+  const withdrawAmount = parseFloat(document.querySelector("#withdrawal-amount").value);
+
+  // to disable the button while fetching topup() or withdraw()
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
 
+  // run topup or withdraw based on user interaction
+  if (document.getElementById("input-amount").value.length != 0){
+    await dbank.topUp(inputAmount);
+  }
+  
+  if (document.getElementById("withdrawal-amount").value.lenth != 0){
+    await dbank.withdraw(withdrawAmount);
+  }
+  
+  // accumulate the time-based interest
+  await dbank.compound();
+
+  update();
+
+  // reset the input field value to empty
+  document.querySelector("#input-amount").value = "";
+  document.querySelector("#withdrawal-amount").value = "";
+  
+  // re enable the submit button for new transaction
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
 });
+
+
+// Reflect the current balance after each transaction
+async function update(){
+  
+  const currentBalance = await dbank.checkBalance();
+  document.getElementById('value').innerHTML = Math.round(currentBalance * 100) / 100;
+}
